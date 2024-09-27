7f5dc7f71fabaeeb73843163c6b71108bfce36fc
WhiceDev <buerger.98@web.de>
2024-09-27 21:20:02 +0200

    + Database load and insert

--- resources/charge/server/Events/PlayerEvents/handleOAuthLogin.js
+++ resources/charge/server/Events/PlayerEvents/handleOAuthLogin.js
@@ -1,5 +1,6 @@
 import * as alt from "alt-server";
 import axios from 'axios';
+import pool from '../../database.js';
 
 alt.onClient('token', async (player, token) => {
     // Validate the token with a GET request to the Discord api
@@ -20,7 +21,31 @@ alt.onClient('token', async (player, token) => {
         return;
     }
 
-    // Example of returned properties
-    alt.log(`Id: ${request.data.id}`);
-    alt.log(`Name: ${request.data.username}#${request.data.discriminator}`);
+    const user = await findUserInDatabase(request.data.id);
+
+    if (user) {
+        alt.log(`User already exists: ${user.discord_name} (${user.discord_id})`);
+        
+    } else {
+        await saveUserToDatabase(request.data.id, request.data.username, request.data.discriminator);
+    }
 });
+
+async function findUserInDatabase(discordId) {
+    try {
+        const [rows] = await pool.query('SELECT * FROM accounts WHERE discord_id = ?', [discordId]);
+        return rows.length > 0 ? rows[0] : null;
+    } catch (error) {
+        alt.logError('Error checking user in database:', error);
+        return null;
+    }
+}
+
+async function saveUserToDatabase(discordId, username, discriminator) {
+    try {
+        const [result] = await pool.query('INSERT INTO accounts (discord_id, discord_name, discriminator) VALUES (?, ?, ?)', [discordId, username, discriminator]);
+        alt.log(`User saved with ID: ${result.insertId}`);
+    } catch (error) {
+        alt.logError('Error saving user to database:', error);
+    }
+}
