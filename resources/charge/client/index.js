3f76caa36f84d76d3b470fece604bb92645513b0
WhiceDev <buerger.98@web.de>
2024-09-27 16:52:53 +0200

    + Player Death Respawn

--- resources/charge/client/index.js
+++ resources/charge/client/index.js
@@ -1,14 +1,25 @@
 import * as alt from "alt-client";
+import { logMessage } from "./log";
 
 
-alt.on('playerConnect', handleConnect);
+alt.on('playerConnect', handlePlayerConnect);
+alt.on('playerDeath', handlePlayerDeath);
 
 
 /**
  * @param {alt.Player} player
  */
-function handleConnect(player) {
+function handlePlayerConnect(player) {
   player.spawn(-1291.71, 83.43, 54.89, 0);
   player.model = 'mp_m_freemode_01';
-  console.log(`+ Player connect (${player.name})`);
+  logMessage(`+ Player connect (${player.name})`);
 }
+
+/**
+ * @param {alt.Player} player
+ */
+function handlePlayerDeath(player) {
+  player.spawn(-1291.71, 83.43, 54.89, 5000);
+  logMessage(`- Player death (${player.name})`);
+}
+
