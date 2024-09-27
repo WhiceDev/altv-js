c30f36fbc254f644389905e1223d6ad0a02af461
WhiceDev <buerger.98@web.de>
2024-09-27 16:36:19 +0200

    + Spawn Player and Set Skin on Connect

--- resources/charge/client/index.js
+++ resources/charge/client/index.js
@@ -1,1 +1,13 @@
 import * as alt from "alt-client";
+
+
+alt.on('playerConnect', handleConnect);
+
+
+/**
+ * @param {alt.Player} player
+ */
+function handleConnect(player) {
+  player.spawn(-1291.71, 83.43, 54.89, 0);
+  player.model = `mp_m_freemode_01`;
+}
