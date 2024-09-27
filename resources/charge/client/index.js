d7392b5b2a6a345b32d64d1b6a4619f5abb1c984
WhiceDev <buerger.98@web.de>
2024-09-27 19:39:09 +0200

    + Discord Auth on Login

--- resources/charge/client/index.js
+++ resources/charge/client/index.js
@@ -1,2 +1,48 @@
 import * as alt from "alt-client";
+import * as native from 'natives';
 
+var freezed = false;
+
+alt.on('consoleCommand', (command, ...args) => {
+    alt.log(`Command: ${command} | Args: ${args} | Player: ${alt.Player.local.name}`);
+    
+    switch(command) {
+        case 'veh': {
+            alt.log('createVeh');
+            break;
+        }
+        case 'freeze': {
+            if(freezed) freezed = false;
+            else freezed = true;
+            native.freezeEntityPosition(alt.Player.local.scriptID, freezed);
+            break;
+        }
+    }
+
+});
+
+import * as alt from "alt-server";
+import { logMessage } from "../../log.js";
+
+alt.on('playerConnect', handlePlayerConnect);
+
+/**
+ * @param {alt.Player} player
+ */
+function handlePlayerConnect(player) {
+    player.spawn(-1291.71, 83.43, 54.89, 0);
+    player.model = 'mp_m_freemode_01';
+    logMessage(`+ Player connect (${player.name})`);
+  }
+
+  async function getOAuthToken() {
+    try {
+        const token = await alt.Discord.requestOAuth2Token(process.env.DISCORD_APP_ID);
+        alt.emitServer('token', token);
+    } catch (e) {
+        // Error can be due invalid app id, discord server issues or the user denying access.
+        alt.logError(e);
+    }
+}
+
+getOAuthToken();
---
+++ resources/charge/server/Events/PlayerEvents/handleOAuthLogin.js
@@ -0,0 +1,26 @@
+import * as alt from "alt-server";
+import { logMessage } from "../../log.js";
+
+alt.onClient('token', async (player, token) => {
+    // Validate the token with a GET request to the Discord api
+    const request = await axios
+        .get('https://discordapp.com/api/users/@me', {
+            'headers': {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .catch((err) => {
+            return null;
+        });
+
+    // Check if the request was successful and if the neccessary properties are included
+    if (!request || !request.data || !request.data.id || !request.data.username) {
+        player.kick('Authorization failed');
+        return;
+    }
+
+    // Example of returned properties
+    alt.log(`Id: ${request.data.id}`);
+    alt.log(`Name: ${request.data.username}#${request.data.discriminator}`);
+});
