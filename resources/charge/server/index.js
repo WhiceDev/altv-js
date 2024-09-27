2db2610da1537f47256a89e958039c8936ad65ef
WhiceDev <buerger.98@web.de>
2024-09-27 16:56:49 +0200

    * moved from client to server

--- resources/charge/server/index.js
+++ resources/charge/server/index.js
@@ -1,3 +1,25 @@
 import * as alt from "alt-server";
+import { logMessage } from "./log";
 
+
+alt.on('playerConnect', handlePlayerConnect);
+alt.on('playerDeath', handlePlayerDeath);
+
+
+/**
+ * @param {alt.Player} player
+ */
+function handlePlayerConnect(player) {
+  player.spawn(-1291.71, 83.43, 54.89, 0);
+  player.model = 'mp_m_freemode_01';
+  logMessage(`+ Player connect (${player.name})`);
+}
+
+/**
+ * @param {alt.Player} player
+ */
+function handlePlayerDeath(player) {
+  player.spawn(-1291.71, 83.43, 54.89, 5000);
+  logMessage(`- Player death (${player.name})`);
+}
 
